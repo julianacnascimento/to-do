a21ae8216d109b49d3b3a5d705b839fd88004477
julianacnascimento <juliana.conceicao@dce.ufpb.br>
2021-10-05 23:14:16 -0300

    subir alteracoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import * as C from "./App.styles"
+
+import { Item } from "./types/item";
+
+export default function App() {
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1, 
+      description: "Example 1",
+      done: false
+    }
+  ])
+
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>To-do List</C.Header>
+      </C.Area>
+    </C.Container>
   );
 }
-
-export default App;
